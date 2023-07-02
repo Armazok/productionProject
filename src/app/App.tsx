1441499bdf4a382204e281ad9340cf51347e41a3
Klaus <nikolai.borsin@mail.ru>
2023-07-02 15:49:41 +0300

    [006].ref component App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import './styles/index.scss'
-import {Suspense} from "react";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -15,12 +13,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная станица</Link>
             <Link to={'/about'}>Не главная страница</Link>
-            <Suspense fallback={`Loading...`}>
-                <Routes>
-                    <Route path='/about' element={<AboutPage />}/>
-                    <Route path='/' element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 }

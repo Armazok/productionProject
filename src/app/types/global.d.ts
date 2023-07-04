678db37ec5462b8dda8c81e94d9c22ce5057c43f
Klaus <nikolai.borsin@mail.ru>
2023-07-04 14:53:36 +0300

    [021].Create loader for debug status i18n

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,7 +6,14 @@ declare module '*.scss' {
     export = classNames;
 }
 
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.svg"
+
 declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
+
+declare const __IS__DEV__: boolean

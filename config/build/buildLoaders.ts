2a45dd27a777411aa58f985c48107a917f5e0cbb
Klaus <nikolai.borsin@mail.ru>
2023-07-04 16:33:33 +0300

    [026].Add webpack loader babelLoader   i18next-extract

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,26 @@ export function buildLoaders({isDev}: IBuildOptions): webpack.RuleSetRule[] {
                 ],
         }
 
+        const babelLoader = {
+            test: /\.(js|jsx|tsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env'],
+                    "plugins": [
+                        [
+                            "i18next-extract",
+                            {
+                                locales: ['ru', 'en'],
+                                keyAsDefaultValue: true
+                            }
+                        ],
+                    ]
+                }
+            }
+        }
+
         const cssLoaders =     {
                 test: /\.s[ac]ss$/i,
                 use: [
@@ -45,8 +65,9 @@ export function buildLoaders({isDev}: IBuildOptions): webpack.RuleSetRule[] {
     }
 
     return  [
-        typescriptLoader,
         cssLoaders,
+        babelLoader,
+        typescriptLoader,
         svgLoader,
         fileLoader
     ]

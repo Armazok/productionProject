697ec6e3ff168d28f1c6d6b4e169b7d7c2c20530
Klaus <nikolai.borsin@mail.ru>
2023-07-13 10:22:19 +0300

    [068].Ref: Modal.tsx Modal.module.scss

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,29 +1,31 @@
 import {
-    FC, MouseEvent, useCallback, useEffect, useRef, useState,
+    MouseEvent, ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
+import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/themeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Modal.module.scss';
 
-interface IModal {
-    className?: string
-    isOpen?: boolean
-    onClose?: () => void
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
 }
 
 const ANIMATION_DELAY = 300;
-export const Modal: FC<IModal> = ({
-    className,
-    children,
-    isOpen,
-    onClose,
-}) => {
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
     const [isClosing, setIsClosing] = useState(false);
-
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
-
-    const onClickContent = (e: MouseEvent) => {
-        e.stopPropagation();
-    };
+    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -35,37 +37,46 @@ export const Modal: FC<IModal> = ({
         }
     }, [onClose]);
 
+    // Новые ссылки!!!
     const onKeyDown = useCallback((e: KeyboardEvent) => {
         if (e.key === 'Escape') {
             closeHandler();
         }
     }, [closeHandler]);
 
+    const onContentClick = (e: MouseEvent) => {
+        e.stopPropagation();
+    };
+
     useEffect(() => {
         if (isOpen) {
             window.addEventListener('keydown', onKeyDown);
         }
+
         return () => {
             clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
         };
     }, [isOpen, onKeyDown]);
 
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
+        [cls[theme]]: true,
     };
+
     return (
-        // <Portal>
-        <div className={classNames(cls.Modal, mods, [className])}>
-            <div className={cls.overlay} onClick={closeHandler}>
-                <div
-                    className={cls.content}
-                    onClick={onClickContent}
-                >
-                    {children}
+        <Portal>
+            <div className={classNames(cls.Modal, mods, [className])}>
+                <div className={cls.overlay} onClick={closeHandler}>
+                    <div
+                        className={cls.content}
+                        onClick={onContentClick}
+                    >
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
-        // </Portal>
+        </Portal>
     );
 };

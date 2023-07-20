c234a2c086b90ca0b0575ba65a350575debb0961
Klaus <nikolai.borsin@mail.ru>
2023-07-20 18:15:44 +0300

    [084].ref:Ref stories

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,10 +1,11 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import ProfilePage from './ProfilePage';
 
 export default {
-    title: 'pages/MainPage',
+    title: 'pages/ProfilePage',
     component: ProfilePage,
     argTypes: {
         backgroundColor: { control: 'color' },
@@ -13,7 +14,8 @@ export default {
 const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...args} />;
 export const Light = Template.bind({});
 Light.arg = {};
+Light.decorators = [StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.arg = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -2,9 +2,11 @@ import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { IStateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { Story } from '@storybook/react';
 import { loginReducers } from 'features/AuthByUserName/model/slice/loginSlice';
+import { profileReducers } from 'entiti/Profile';
 
 const defaultAsyncReducer: DeepPartial<ReducersMapObject<IStateSchema>> = {
     loginForm: loginReducers,
+    profile: profileReducers,
 };
 export const StoreDecorator = (
     state: DeepPartial<IStateSchema>,

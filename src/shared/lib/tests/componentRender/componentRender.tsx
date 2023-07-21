8eb12d404b46118b1b375a571e0666cdb1303525
Klaus <nikolai.borsin@mail.ru>
2023-07-21 21:56:32 +0300

    [092].fix tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -16,12 +16,13 @@ export function ComponentRender(component: ReactNode, options: IComponentRenderO
         initialState,
     } = options;
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTest}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>
+        ,
     );
 }

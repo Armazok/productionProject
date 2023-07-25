5412c4d1019a7f1cce09e594ff829bc6791ddab9
Klaus <nikolai.borsin@mail.ru>
2023-07-25 12:20:50 +0300

    [100].Update user authentication and initial navigation flow
    This commit modifies the user authentication and initial navigation architecture of the application. The changes include export modification of AppRouteProps in RouteConfig.tsx, addition of '_inited' to the IUserSchema interface and usage of useCallback on AppRouter.tsx.
    
    Additionally, a new selector for user inited state and a requireAuth component for checking user's auth state are introduced. Moreover, the App.tsx file now checks user's authentication state before rendering the AppRouter component. Finally, an initial state '_inited' flag is added to the userSlice.ts file.
    
    These changes increase code maintainability, provide better user experience by handling authentication states, and improve the speed of first navigation by directly rendering the respective component based on user's auth state.

--- src/entity/User/index.ts
+++ src/entity/User/index.ts
@@ -1,3 +1,4 @@
+export { getUserInited } from './model/selectors/getUser:_inited/getUserInited';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
 export { IUserSchema, IUser } from './model/types/user';
 export { userReducers, userActions } from './model/slice/userSlice';
